e2c043051e72da4e8fe980cde9a14973269615e7
kraynuk.m <kraynuk.m@2052384.no-reply.drupal.org>
2017-06-25 15:20:36 +0200

    Issue #2866235 by mikhail.krainiuk: Drupal 8? (fix)

--- src/Plugin/views/field/ViewsDateFormatSqlField.php
+++ src/Plugin/views/field/ViewsDateFormatSqlField.php
@@ -1,59 +1,280 @@
 <?php
-/**
- * @file
- * This file contains the class to override views_handler_field_date.
- */
+
+namespace Drupal\views_date_format_sql\Plugin\views\field;
+
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\views\ResultRow;
+use Drupal\views\Plugin\views\field\Field;
+use Drupal\Core\Datetime\Entity\DateFormat;
+use Drupal\Core\Datetime\DateFormatter;
+use Drupal\Core\Render\BubbleableMetadata;
+use Drupal\Core\Render\Element;
 
 /**
- * Allows to removes the date formatting from render() and put it in query().
+ * A field that displays entity timestamp field data. Supports grouping.
+ *
+ * @ingroup views_field_handlers
+ *
+ * @ViewsField("views_date_format_sql_field")
  */
-class views_date_format_sql_handler_date extends views_handler_field_date {
+class ViewsDateFormatSqlField extends Field {
 
-  function option_definition() {
-    $options = parent::option_definition();
+  private $format;
+  private $format_string;
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function defineOptions() {
+    $options = parent::defineOptions();
 
     $options['format_date_sql'] = array('default' => FALSE);
 
     return $options;
   }
 
-  function options_form(&$form, &$form_state) {
-    parent::options_form($form, $form_state);
+  /**
+   * {@inheritdoc}
+   */
+  public function buildOptionsForm(&$form, FormStateInterface $form_state) {
 
     $form['format_date_sql'] = array(
       '#type' => 'checkbox',
-      '#title' => t('Use SQL to format date'),
-      '#description' => t('Use the SQL database to format the date. This enables date values to be used in grouping aggregation.'),
+      '#title' => $this->t('Use SQL to format date'),
+      '#description' => $this->t('Use the SQL databse to format the date. This enables date values to be used in grouping aggregation.'),
       '#default_value' => $this->options['format_date_sql'],
     );
+
+    parent::buildOptionsForm($form, $form_state);
   }
 
-  function query() {
-    if ($this->options['format_date_sql'] != TRUE) {
-      return parent::query();
+  /**
+   * Called to add the field to a query.
+   *
+   * By default, all needed data is taken from entities loaded by the query
+   * plugin. Columns are added only if they are used in groupings.
+   */
+  public function query($use_groupby = FALSE) {
+    if (empty($this->options['format_date_sql'])) {
+      return parent::query($use_groupby);
     }
-    $this->ensure_my_table();
-    // Add the field.
-    $params = $this->options['group_type'] != 'group' ? array('function' => $this->options['group_type']) : array();
 
-    $format = $this->options['date_format'];
-    $custom_format = $this->options['custom_date_format'];
-    $format_string = _views_date_format_sql_get_date_format($format, $custom_format);
+    $fields = $this->additional_fields;
+    // No need to add the entity type.
+    $entity_type_key = array_search('entity_type', $fields);
+    if ($entity_type_key !== FALSE) {
+      unset($fields[$entity_type_key]);
+    }
 
-    $formula = views_date_sql_format($format_string, "$this->table_alias.$this->real_field");
+    if ($use_groupby) {
+      // Add the fields that we're actually grouping on.
+      $options = array();
+      if ($this->options['group_column'] != 'entity_id') {
+        $options = array($this->options['group_column'] => $this->options['group_column']);
+      }
+      $options += is_array($this->options['group_columns']) ? $this->options['group_columns'] : array();
 
-    $this->field_alias = $this->query->add_field(NULL, $formula, "{$this->table_alias}_{$this->real_field}", $params);
-    $this->query->add_groupby($this->field_alias);
+      // Go through the list and determine the actual column name from field api.
+      $fields = array();
+      $table_mapping = $this->getTableMapping();
+      $field_definition = $this->getFieldStorageDefinition();
 
-    $this->add_additional_fields();
+      foreach ($options as $column) {
+        $fields[$column] = $table_mapping->getFieldColumnName($field_definition, $column);
+      }
+
+      $this->group_fields = $fields;
+    }
+
+    // Add additional fields (and the table join itself) if needed.
+    if ($this->add_field_table($use_groupby)) {
+      $this->ensureMyTable();
+      $this->setDateFormat();
+
+      // Add the field.
+      $params = $this->options['group_type'] !== 'group' ? array('function' => $this->options['group_type']) : array();
+
+      $formula = $this->query->getDateFormat("FROM_UNIXTIME($this->tableAlias.$this->realField)", $this->format_string);
+
+      $this->field_alias = $this->query->addField(NULL, $formula, "{$this->tableAlias}_{$this->realField}", $params);
+      $this->query->addGroupBy($this->field_alias);
+
+      $this->aliases[$this->realField] = $this->field_alias;
+    }
+    else {
+      $this->setDateFormat();
+    }
+
+    // Let the entity field renderer alter the query if needed.
+    $this->getEntityFieldRenderer()->query($this->query, $this->relationship);
   }
 
-  function render($values) {
-    if ($this->options['format_date_sql'] != TRUE) {
-      return parent::render($values);
+  /**
+   * Sets date format from field options.
+   */
+  protected function setDateFormat() {
+    if (empty($this->options['settings']['date_format'])) {
+      $this->format = '';
+      $this->format_string = '';
+      return;
     }
-    $format = $this->options['date_format'];
-    return $this->get_value($values);
+
+    $this->format = $this->options['settings']['date_format'];
+    if ($this->format === 'custom') {
+      $this->format_string = !empty($this->options['settings']['custom_date_format'])
+          ? $this->options['settings']['custom_date_format']
+          : '';
+    }
+    else {
+      /* @var DateFormat $formatter */
+      $formatter = DateFormat::load($this->format);
+      $this->format_string = empty($formatter) ? '' : $formatter->getPattern();
+    }
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getValue(ResultRow $values, $field = NULL) {
+    if (empty($this->options['format_date_sql'])) {
+      return parent::getValue($values, $field);
+    }
+
+    $entity = $this->getEntity($values);
+    // Some bundles might not have a specific field, in which case the entity
+    // (potentially a fake one) doesn't have it either.
+    /** @var \Drupal\Core\Field\FieldItemListInterface $field_item_list */
+    $field_item_list = isset($entity->{$this->definition['field_name']}) ? $entity->{$this->definition['field_name']} : NULL;
+
+    if (!isset($field_item_list)) {
+      // Check empty date field for "empty rows".
+      if (!empty($values->is_empty_row)) {
+        // Render empty date.
+        if (isset($this->field_alias) && !empty($values->{$this->field_alias})) {
+          return $values->{$this->field_alias};
+        }
+      }
+
+      // There isn't anything we can do without a valid field.
+      return NULL;
+    }
+
+    $field_item_definition = $field_item_list->getFieldDefinition();
+
+    $values = [];
+    foreach ($field_item_list as $field_item) {
+      /** @var \Drupal\Core\Field\FieldItemInterface $field_item */
+      if ($field) {
+        $values[] = $field_item->$field;
+      }
+      // Find the value using the main property of the field. If no main
+      // property is provided fall back to 'value'.
+      elseif ($main_property_name = $field_item->mainPropertyName()) {
+        $values[] = $field_item->{$main_property_name};
+      }
+      else {
+        $values[] = $field_item->value;
+      }
+    }
+
+    /* @var DateFormatter $dateFormatter */
+    $dateFormatter = \Drupal::service('date.formatter');
+
+    if ($field_item_definition->getFieldStorageDefinition()->getCardinality() == 1) {
+      $timestamp = reset($values);
+
+      if (empty($this->format)) {
+        return $timestamp;
+      }
+
+      return $dateFormatter->format($timestamp, $this->format, $this->format_string);
+    }
+    else {
+      if (empty($this->format)) {
+        return $values;
+      }
+
+      foreach ($values as &$value) {
+        $value = $dateFormatter->format($value, $this->format, $this->format_string);
+      }
+      return $values;
+    }
+  }
+
+  /**
+   * Called to determine what to tell the clicksorter.
+   */
+  public function clickSort($order) {
+    if (empty($this->options['format_date_sql'])) {
+      return parent::clickSort($order);
+    }
+
+    // No column selected, can't continue.
+    if (empty($this->options['click_sort_column'])) {
+      return NULL;
+    }
+
+    $this->ensureMyTable();
+    $field_storage_definition = $this->getFieldStorageDefinition();
+    $column = $this->getTableMapping()->getFieldColumnName($field_storage_definition, $this->options['click_sort_column']);
+    if (!isset($this->aliases[$column])) {
+      // Column is not in query; add a sort on it (without adding the column).
+      $this->aliases[$column] = $this->tableAlias . '.' . $column;
+    }
+    $this->query->addOrderBy(NULL, NULL, $order, $this->aliases[$column]);
+
+    // Added to group ungrouped "timestamp" fields. Can occurs when
+    // $this->addAdditionalFields is called with NULL $fields param.
+    if ($this->view->display_handler->useGroupBy()
+      && array_search($this->aliases[$column], $this->query->groupby, TRUE) === FALSE
+    ) {
+      $this->query->addGroupBy($this->aliases[$column]);
+    }
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getItems(ResultRow $values) {
+    if (empty($this->options['format_date_sql'])) {
+      return parent::getItems($values);
+    }
+
+    if (!$this->displayHandler->useGroupBy()) {
+      $build_list = $this->getEntityFieldRenderer()->render($values, $this);
+    }
+    else {
+      // Render date values from SQL result.
+      $alias = $this->aliases[$this->definition['field_name']];
+      return [['rendered' => $values->{$alias}]];
+    }
+
+    // Code from parent function.
+    if (!$build_list) {
+      return [];
+    }
+
+    if ($this->options['field_api_classes']) {
+      return [['rendered' => $this->renderer->render($build_list)]];
+    }
+
+    // Render using the formatted data itself.
+    $items = [];
+    // Each item is extracted and rendered separately, the top-level formatter
+    // render array itself is never rendered, so we extract its bubbleable
+    // metadata and add it to each child individually.
+    $bubbleable = BubbleableMetadata::createFromRenderArray($build_list);
+    foreach (Element::children($build_list) as $delta) {
+      BubbleableMetadata::createFromRenderArray($build_list[$delta])
+        ->merge($bubbleable)
+        ->applyTo($build_list[$delta]);
+      $items[$delta] = [
+        'rendered' => $build_list[$delta],
+        // Add the raw field items (for use in tokens).
+        'raw' => $build_list['#items'][$delta],
+      ];
+    }
+    return $items;
   }
 
 }
